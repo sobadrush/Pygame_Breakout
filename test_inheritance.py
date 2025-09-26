3223412800ba15967d68a9d4042f236061a0b3b5
Roger Lo <sobadrush@icloud.com>
2025-09-26 22:57:22 +0800

    [新增] (test_inheritance.py): 添加動物類別及其子類別
    1. 實現了 Animal 類別及其子類別 Horse、Donkey 和 Mule
    2. 每個類別都有自己的 speak 方法以顯示不同的聲音

---
+++ test_inheritance.py
@@ -0,0 +1,38 @@
+class Animal:
+  def __init__(self):
+    self.weight = 50
+  
+  def speak(self):
+    print("wa wa wa")
+    
+########################### 
+    
+class Horse(Animal):
+  def __init__(self):
+    super().__init__()
+  
+  def speak(self):
+    print("La La La")
+
+###########################
+
+class Donkey(Animal):
+  def __init__(self):
+    super().__init__()
+  
+  def speak(self):
+    print("ear ear")
+    
+###########################
+    
+class Mule(Animal):
+  def __init__(self):
+    super().__init__()
+    
+  def speak(self):
+    print("一齁一齁一齁")
+  
+###########################
+
+my_mule = Mule()
+my_mule.speak()
